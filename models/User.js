53521d34d647f6e83482e315edaf98ad72c20606
Dennys Marcos <kardeenx@gmail.com>
2021-02-03 19:01:05 -0300

    bcrypt and hooks to hash pwd

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,6 +16,19 @@ const userSchema = new mongoose.Schema({
         minlength: [6, 'The password needs at least 6 characters.']
     },
 });
+
+// Calls this function before the event - e.g. before the save event:
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+// Calls this function after the event - e.g. post the save event:
+// userSchema.post('save', ( doc, next ) => {
+//     console.log('new user was created and saved', doc)
+//     next();
+// })
 
 const User = mongoose.model('user', userSchema);
 
