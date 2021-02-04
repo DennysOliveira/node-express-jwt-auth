c0ed70f10dce72ea6b51c19bd0d177ecf241332e
Dennys Marcos <kardeenx@gmail.com>
2021-02-04 13:44:05 -0300

    added warning needs to be logged in to see the restricted page

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,21 +1,27 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
+var tempRedirected = false;
+
 function requireAuth ( req, res, next ){
     const token = req.cookies.jwt;
 
     // checks JWT exists
-    if (token) {
+    if (token) { // if it exists, check for JWT authenticity
         jwt.verify(token, process.env.JWT_SECRET, ( err, decodedToken ) => {
-            if (err) {
+            if (err) { // if it fails verification
+                tempRedirected = true;
+                
                 console.log(err.message);
                 res.redirect('/login');
-            } else {
+            } else { // passes verification successfully a.k.a. correctly authenticated
+                tempRedirected = false;
                 console.log(decodedToken);
                 next();
             }
         })
-    } else {
+    } else { // if JWT is non-existence aka user not logged in
+        tempRedirected = true;
         res.redirect('/login');
     }
 };
@@ -23,20 +29,29 @@ function requireAuth ( req, res, next ){
 // check current user 
 function checkUser ( req, res, next ) {
     const token = req.cookies.jwt;
-
+    
     if (token) {
         jwt.verify(token, process.env.JWT_SECRET, async ( err, decodedToken ) => {
             if (err) {
+                if (tempRedirected) {
+                    res.locals.redirected = true;
+                }
+                tempRedirected = false;
+
                 console.log(err.message);
                 res.locals.user = null;
                 next();
-            } else {
+            } else { // If the user exists and it's auth token is verified correctly
                 let user = await User.findById(decodedToken.id);
                 res.locals.user = user;
                 next();
             }
         })
     } else {
+        if (tempRedirected) {
+            res.locals.redirected = true;
+        }
+        tempRedirected = false;
         res.locals.user = null;
         next();
     }
